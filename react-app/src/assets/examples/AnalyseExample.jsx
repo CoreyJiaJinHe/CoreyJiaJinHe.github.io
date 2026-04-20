9e214f6c2e3d5f78852448b12684818042afbfc5
Corey He <corey72he@gmail.com>
2026-04-20 15:26:48 -0400

    Refactor: Apply consistent link styling in AnalyseExample component for improved readability

--- react-app/src/assets/examples/AnalyseExample.jsx
+++ react-app/src/assets/examples/AnalyseExample.jsx
@@ -1,9 +1,15 @@
 function AnalyseExample() {
+  const linkStyle = {
+    color: 'inherit',
+    textDecoration: 'underline',
+    fontWeight: 600,
+  }
+
   return (
     <p>
-      Raw Dataset found here: <a href="https://archive.ics.uci.edu/dataset/529/early+stage+diabetes+risk+prediction+data/">Early Stage Diabetes Risk Prediction Dataset</a>
+      Raw Dataset found here: <a style={linkStyle} href="https://archive.ics.uci.edu/dataset/529/early+stage+diabetes+risk+prediction+data/">Early Stage Diabetes Risk Prediction Dataset</a>
       <br /><br />
-      Full report can be read from this <a href="https://docs.google.com/document/d/1xrk8EbClqNyVbGpnO0y5ip0V2DVDw0DN/edit?usp=sharing&ouid=111487749005686481730&rtpof=true&sd=true">link</a>
+      Full report can be read from this <a style={linkStyle} href="https://docs.google.com/document/d/1xrk8EbClqNyVbGpnO0y5ip0V2DVDw0DN/edit?usp=sharing&ouid=111487749005686481730&rtpof=true&sd=true">link</a>
       <br /><br />
       Summary: Analysis of a Dataset filled with Patient Data to create a model to detect the onset of diabetes in their early stages through a number of associated symptoms.
       <br /><br />
